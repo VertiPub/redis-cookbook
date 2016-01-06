7367f8cdaf135c5a02046c744d3aeaae042a8acb
Rob Parker <rparker@altiscale.com>
2016-01-06 15:10:19 -0600

    SI-514 add name to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name             "redis"
 maintainer       "Riot Games"
 maintainer_email "jwinsor@riotgames.com"
 license          "Apache 2.0"
 description      "Installs and configures redis"
-version          "0.9.3"
+version          "0.9.4"
 
 supports "centos", ">= 5.5"
